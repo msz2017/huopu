24bcc0b5d92f369aa546b98a802da3b661a2c96e
shuz <15558057209@163.com>
2018-05-21 21:32:35 +0800

    totast

--- example/index.js
+++ example/index.js
@@ -1,1 +1,8 @@
 console.log(huoui);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toast = document.getElementById('toast');
+    toast.addEventListener('click',function(evt){
+        huoui.toast('我就试试不干嘛！');
+    })
+})
--- src/toast/toast.js
+++ src/toast/toast.js
@@ -1,13 +1,18 @@
 import $ from '../util'
 import tpl from './toast.html'
 const toast = (msg = '', { duration = 2000 } = {}) => {
-    debugger
     const $tpl = $.render(tpl, { msg });
     $('body').append($tpl);
+    setTimeout(() => {
+        $tpl.css({ opacity: 1 })
+    }, 10);
+    setTimeout(() => {
+        $tpl.css({ opacity: 0 })
+    }, duration);
 
     setTimeout(() => {
         $tpl.remove();
-    }, duration);
+    }, duration + 300);
 }
 
 export default toast;
