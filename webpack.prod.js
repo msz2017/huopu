a2e73397a7ae1926f6ba0d63958b9b90803fe421
shuz <15558057209@163.com>
2018-05-22 16:09:41 +0800

    BundleAnalyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,12 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const common = require('./webpack.config');
 const merge = require('webpack-merge');
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 const config = {
     mode: 'production',
+    plugins: [new BundleAnalyzerPlugin()],
     optimization: {
         minimizer: [
             new UglifyJsPlugin({
